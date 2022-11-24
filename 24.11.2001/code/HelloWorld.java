8282d9db3f1957a739a5b2a270d137d1a54f5419
sharmilaroja <118958387+sharmilaroja@users.noreply.github.com>
2022-11-24 12:02:12 +0530

    Add files via upload

---
+++ 24.11.2001/code/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld
+{
+	public static void main(String []args)
+	{
+		System.out.println("Hello java ");
+		System.out.println(" i am sharmila");
+		System.out.println("number: 7904233785");
+		System.out.println("Address keelkatalai");
+		System.out.println("email id: sharmilasharmila241@gmail.com");
+	}
+
+}
